1d732dd0e27067fffca35c8fcc6a8d652b270c72
Sadjad Bazarnovi <sadjad.bzr@gmail.com>
2020-07-28 13:44:18 +0430

    define game function and replace temporary points list with one variable (hakem point)

--- shelem points.py
+++ shelem points.py
@@ -10,8 +10,6 @@ names_list = names()
 team1 = names_list[0].lower(), names_list[1].lower(), '1', 'one'
 team2 = names_list[2].lower(), names_list[3].lower(), '2', 'two'
 
-team1_points = 0
-team2_points = 0
 
 print(f'''Team 1: {team1[:2]}
 Team 2: {team2[:2]}''')
@@ -57,43 +55,43 @@ def declarer_bid():
     return declarer, bid
 
 
-print('let\'s start the game!')
-n = 1
-while True:
-    if team2_points < goal and team1_points < goal:
-        print(f'game {n}')
-        hakem, hokm = declarer_bid()
-        while True:
-            try:
-                hammal_point = int(input('The other team (not declarer) points: '))
-                if hammal_point % 5 or hammal_point < 0 or hammal_point > 165:
-                    print('this score is not acceptable')
-                else:
-                    break
-            except ValueError:
-                print('It should be a number!')
-        point = [0, 0]
-        point[1] = hammal_point
-        if hammal_point == 0:
-            point[0] = 2 * hokm
-        elif hammal_point <= (165 - hokm):
-            point[0] = hokm
-        elif hammal_point < 85:
-            point[0] = -hokm
-        elif hammal_point >= 85:
-            point[0] = -2 * hokm
+def game():
+    team1_points = 0
+    team2_points = 0
+    print('let\'s start the game!')
+    n = 1
+    while True:
+        if team2_points < goal and team1_points < goal:
+            print(f'game {n}')
+            hakem, hokm = declarer_bid()
+            while True:
+                try:
+                    hammal_point = int(input('The other team (not declarer) points: '))
+                    if hammal_point % 5 or hammal_point < 0 or hammal_point > 165:
+                        print('this score is not acceptable')
+                    else:
+                        break
+                except ValueError:
+                    print('It should be a number!')
+            hakem_point = 0
+            if hammal_point == 0:
+                hakem_point = 2 * hokm
+            elif hammal_point <= (165 - hokm):
+                hakem_point = hokm
+            elif hammal_point < 85:
+                hakem_point = -hokm
+            elif hammal_point >= 85:
+                hakem_point = -2 * hokm
 
-        # point[0] = hakem point
-        # point[1] = hammal point
-        if hakem.lower() in team1:
-            team1_points += point[0]
-            team2_points += point[1]
-        elif hakem.lower() in team2:
-            team2_points += point[0]
-            team1_points += point[1]
-        print(f'''Team 1 {team1[:2]} score = {team1_points}
-Team 2 {team2[:2]} score = {team2_points}''')
-        n += 1
-    else:
-        print('WIN')
-        break
+            if hakem.lower() in team1:
+                team1_points += hakem_point
+                team2_points += hammal_point
+            elif hakem.lower() in team2:
+                team2_points += hakem_point
+                team1_points += hammal_point
+            print(f'''Team 1 {team1[:2]} score = {team1_points}
+    Team 2 {team2[:2]} score = {team2_points}''')
+            n += 1
+        else:
+            print('Win')
+            break
